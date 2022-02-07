6d1da48f58678a9ff62f28ad118a3b9be7ec44d1
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2022-02-07 17:56:20 +0530

    Create max_in_generic_tree.java

---
+++ generic tree/max_in_generic_tree.java
@@ -0,0 +1,10 @@
+private static int max(Node node) {
+  int m = Integer.MIN_VALUE;
+
+  for (Node child : node.children) {
+    int cm = max(child);
+    m = Math.max(m, cm);
+  }
+  m = Math.max(m, node.data);
+  return m;
+}
