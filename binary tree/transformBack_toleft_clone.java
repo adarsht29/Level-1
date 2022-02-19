54eaa1311b789f74c8b42921a91302712c2d375a
Adarsh Thoke <37337770+adarshx06@users.noreply.github.com>
2022-02-19 17:12:10 +0530

    Update transformBack_toleft_clone.java

--- binary tree/transformBack_toleft_clone.java
+++ binary tree/transformBack_toleft_clone.java
@@ -1,1 +1,13 @@
 
+  public static Node transformbackLeftCloneTree(Node node){
+    
+    if(node==null){
+        return  null;
+    }
+    Node lcr =  transformbackLeftCloneTree(node.left.left);
+    Node rcr =  transformbackLeftCloneTree(node.right);
+    
+    node.left=lcr;
+    node.right=rcr;
+    return node;
+  }
